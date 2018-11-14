1d05d1811d0e8121da482927605ff288cd2d4435
fengqijie <fengqijie@guazi.com>
2018-11-14 10:29:51 +0800

    demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,216 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="main" style="width: 100%; height:500px;"></div>
 </template>
 
 <script>
+import $echarts from 'echarts';
+import cityMap from '@/js/citymap.js';
+import $axios from 'axios';
+
+const provinces = {
+    //23个省
+    "台湾": "taiwan",
+    "河北": "hebei",
+    "山西": "shanxi",
+    "辽宁": "liaoning",
+    "吉林": "jilin",
+    "黑龙江": "heilongjiang",
+    "江苏": "jiangsu",
+    "浙江": "zhejiang",
+    "安徽": "anhui",
+    "福建": "fujian",
+    "江西": "jiangxi",
+    "山东": "shandong",
+    "河南": "henan",
+    "湖北": "hubei",
+    "湖南": "hunan",
+    "广东": "guangdong",
+    "海南": "hainan",
+    "四川": "sichuan",
+    "贵州": "guizhou",
+    "云南": "yunnan",
+    "陕西": "shanxi1",
+    "甘肃": "gansu",
+    "青海": "qinghai",
+    //5个自治区
+    "新疆": "xinjiang",
+    "广西": "guangxi",
+    "内蒙古": "neimenggu",
+    "宁夏": "ningxia",
+    "西藏": "xizang",
+    //4个直辖市
+    "北京": "beijing",
+    "天津": "tianjin",
+    "上海": "shanghai",
+    "重庆": "chongqing",
+    //2个特别行政区
+    "香港": "xianggang",
+    "澳门": "aomen"
+};
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    data() {
+        return {
+            special: ["北京","天津","上海","重庆","香港","澳门"],
+            mapdata: [],
+            option: {
+                title: {
+                    text: '全国终端分布',
+                    subtext: '下钻',
+                    link: 'https://blog.csdn.net/example440982',
+                    left: 'center',
+                    textStyle: {
+                        color: '#fff',
+                        fontSize: 16,
+                        fontWeight: 'normal',
+                        fontFamily: "Microsoft YaHei"
+                    },
+                    subtextStyle: {
+                        color: '#ccc',
+                        fontSize: 13,
+                        fontWeight: 'normal',
+                        fontFamily: "Microsoft YaHei"
+                    }
+                },
+                tooltip: {
+                    trigger: 'item',
+                    formatter: '{b}'
+                },
+                toolbox: {
+                    show: true,
+                    orient: 'vertical',
+                    left: 'right',
+                    top: 'center',
+                    feature: {
+                        dataView: {readOnly: false},
+                        restore: {},
+                        saveAsImage: {}
+                    },
+                    iconStyle: {
+                        normal: {
+                            color: '#fff'
+                        }
+                    }
+                },
+                animationDuration: 1000,
+                animationEasing: 'cubicOut',
+                animationDurationUpdate: 1000,
+                visualMap: {
+                  type: 'continuous',
+                  min: 0,
+                  max: 300,
+                  text:['High','Low'],
+                  realtime: false,
+                  calculable : true,
+                  color: ['orangered','yellow','lightskyblue']
+              },
+            },
+        }
+    },
+    methods: {
+        initChart() {
+            let chart = $echarts.init(document.getElementById('main'));
+            let _self = this;
+            $axios.get('/map/china.json')
+                .then(function(res){
+                    let d = [];
+                    for( var i=0; i<res.data.features.length; i++ ){
+                        d.push({
+                            name: res.data.features[i].properties.name,
+                            value: Math.random()*100
+                        })
+                    }
+                    _self.mapdata = d;
+                    //注册地图
+                    $echarts.registerMap('china', res.data);
+                    //绘制地图
+                    _self.renderMap('china',d);
+                });
+            //地图点击事件
+            chart.on('click', function (params) {
+                // console.log( params );
+                if( params.name in provinces ){
+                    //如果点击的是34个省、市、自治区，绘制选中地区的二级地图
+                    $axios.get('/map/province/'+ provinces[params.name] +'.json')
+                        .then(function(res){
+                            $echarts.registerMap( params.name, res.data );
+                            var d = [];
+                            for( var i=0; i<res.data.features.length; i++ ){
+                                d.push({
+                                    name: res.data.features[i].properties.name
+                                })
+                            }
+                            _self.renderMap(params.name, d);
+                        });
+                }else if( params.seriesName in provinces ){
+                    //如果是【直辖市/特别行政区】只有二级下钻
+                    if( _self.special.indexOf( params.seriesName ) >= 0 ){
+                        _self.renderMap('china', _self.mapdata);
+                    }else{
+                        //显示县级地图
+                        $axios.get('/map/city/'+ cityMap[params.name] +'.json')
+                            .then(function(res){
+                                $echarts.registerMap( params.name, res.data );
+                                var d = [];
+                                for( var i=0; i<res.data.features.length; i++ ){
+                                    d.push({
+                                        name: res.data.features[i].properties.name
+                                    })
+                                }
+                                _self.renderMap(params.name,d);
+                            });	
+                    }	
+                }else{
+                    _self.renderMap('china',this.mapdata);
+                }
+            });
+        },
+        renderMap(map, data) {
+            let chart = $echarts.init(document.getElementById('main'));
+            this.option.title.subtext = map;
+            this.option.series = [ 
+                {
+                    name: map,
+                    type: 'map',
+                    mapType: map,
+                    roam: false,
+                    nameMap:{
+                        'china':'中国'
+                    },
+                    label: {
+                        normal:{
+                            show:true,
+                            textStyle:{
+                                color:'#999',
+                                fontSize:13
+                            }  
+                        },
+                        emphasis: {
+                            show: true,
+                            textStyle:{
+                                color:'#fff',
+                                fontSize:13
+                            }
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            areaColor: '#323c48',
+                            borderColor: 'dodgerblue'
+                        },
+                        emphasis: {
+                            areaColor: 'darkorange'
+                        }
+                    },
+                    data:data
+                }	
+            ];
+            //渲染地图
+            chart.setOption(this.option);
+        }
+    },
+    mounted() {
+        this.initChart();
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
